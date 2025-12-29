b325551633b6756ddbb3b7f2cc94b6f3f22c0605
pmeena0934-blip <pmeena0934@gmail.com>
2025-12-29 19:26:04 +0530

    Add initial CSS styles for the application

---
+++ style.js
@@ -0,0 +1,25 @@
+body {
+  font-family: Arial;
+  text-align: center;
+  background: #000;
+  color: #fff;
+}
+
+.box {
+  margin-top: 30px;
+}
+
+input {
+  display: block;
+  margin: 10px auto;
+  padding: 10px;
+  width: 80%;
+}
+
+button {
+  padding: 10px 20px;
+  margin: 5px;
+  background: #111;
+  color: white;
+  border: 1px solid #444;
+}
